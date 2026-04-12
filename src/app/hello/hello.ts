7c0331544f60be3a6b1a0087f97a1570c37c6d58
Mariana <marianasantana1008@gmail.com>
2026-04-12 17:56:34 -0300

    feat: update project structure and add Hello component
    - Added `schematics-scss-migrate` dependency to package.json.
    - Removed unused app.css and styles.css files.
    - Created app.scss for styling and updated app.ts to use it.
    - Introduced a new Hello component with its own HTML, SCSS, and spec files.
    - Updated app.html to include the Hello component.
    - Removed the title rendering test from app.spec.ts.
    - Implemented signal-based state management in the Hello component.

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,36 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+  protected title = 'Welcome to Modern Angular!';
+  protected isDisabled = false;
+
+  protected onClick() {
+    console.log('button click');
+    this.isDisabled = !this.isDisabled;
+  }
+
+  protected count = signal(0);
+  protected doubleCount = computed(() => this.count() * 2);
+
+  private readonly countLog = effect(() => {
+    console.log('Count changed:', this.count());
+  });
+
+  protected increateCounter() {
+    this.count.update((value) => value + 1);
+  }
+
+  protected decreaseCounter() {
+    this.count.update((value) => value - 1);
+  }
+
+  protected reseteCounter() {
+    this.count.set(0);
+  }
+}
