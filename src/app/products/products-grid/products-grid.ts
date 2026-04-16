2c8872645836bced7848726ed4ac224add5b33de
Mariana <marianasantana1008@gmail.com>
2026-04-16 19:51:51 -0300

    Modern Angular - enhance product card with sale badge and price display; - implement search functionality in products grid

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,36 +1,54 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../product';
 import { MatIcon } from "@angular/material/icon";
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIcon],
+  imports: [ProductCard, MatIcon, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
 
+  protected readonly searchTerm = signal('');
+
   protected readonly products = signal<Product[]>([
     {
       id: 1,
-      name: 'Product 1',
-      description: 'Description for Product 1',
+      name: 'Product 1 something something',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
       price: 19.99
     },
     {
       id: 2,
       name: 'Product 2',
-      description: 'Description for Product 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
       price: 29.99,
       originalPrice: 39.99
     },
     {
       id: 3,
       name: 'Product 3',
-      description: 'Description for Product 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
       price: 9.99
     }
   ]);
+
+  protected readonly filteredProducts = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+    if(!term) return this.products();
+
+    return this.products().filter((product) =>
+      product.name.toLowerCase().includes(term) ||
+      product.description.toLocaleLowerCase().includes(term)
+    );
+  });
+
+  protected clearSearch() {
+    this.searchTerm.set('');
+  }
 
 }
